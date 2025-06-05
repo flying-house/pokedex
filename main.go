c4cf6614bc39e9d1d0c079bcd50a7f3a7297c6a2
kylehouseholder <kylemhouseholder@gmail.com>
2025-06-05 12:47:53 -0500

    chore: remove tracked file & added gitignore

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
+)
+
+const (
+	apiURL = "https://pokeapi.co/api/v2/pokemon"
 )
 
 func main() {
@@ -32,6 +39,28 @@ func main() {
 			}
 		}
 	}
+}
+
+func getMapLocations() ([]string, error) {
+	response, err := http.Get(apiURL)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	var data map[string]any
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+
 }
 
 func cleanInput(text string) []string {
